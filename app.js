97b72545d9c35edc56ad2481713dedbed05d16e9
Yasmin <yasmin.9399978@aluno.mg.gov.br>
2026-04-15 16:39:53 +0000

    aa

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ import {
   remove
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
 
+// SUA CONFIGURAÇÃO REAL
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  databaseURL: "SUA_DATABASE_URL",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.firebasestorage.app",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyCOsIklRs-PtT1kxHQ5My-CUgd1n7mzUnI",
+  authDomain: "teste-6f017.firebaseapp.com",
+  databaseURL: "https://teste-6f017-default-rtdb.firebaseio.com/",
+  projectId: "teste-6f017",
+  storageBucket: "teste-6f017.firebasestorage.app",
+  messagingSenderId: "331373288049",
+  appId: "1:331373288049:web:5ae39c50baac6490f2d9d0",
+  measurementId: "G-6C2EWKD698"
 };
 
 const app = initializeApp(firebaseConfig);
